9c7ded2bbb461c79c69d2a57fd331d06c34664cc
gyl327 <2950903389@qq.com>
2021-05-30 17:36:46 +0800

    改进了欢迎引导页面，第一次打开软件进入欢迎引导页面和登录注册页面，再次打开软件直接进入电商首页

---
+++ app/src/main/java/com/example/myapp/LaunchWelcomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchWelcomeActivity extends AppCompatActivity implements  Runnable{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_welcome);
+        new Thread(this).start();
+    }
+    @Override
+    public void run() {
+        try {
+            //延迟1秒时间
+            Thread.sleep(1000);
+            SharedPreferences preferences= getSharedPreferences("count", 0); // 存在则打开它，否则创建新的Preferences
+            int count = preferences.getInt("count", 0); // 取出数据
+
+            /**
+             *如果用户不是第一次使用则直接调转到显示界面,否则调转到引导界面
+             */
+            if (count == 0){
+                Intent intent1 = new Intent();
+                intent1.setClass(this, LaunchImproveActivity.class);
+                startActivity(intent1);
+            }else {
+                Intent intent2 = new Intent();
+                intent2.setClass(this, DepartmentStoreActivity.class);
+                startActivity(intent2);
+            }
+            finish();
+
+            //实例化Editor对象
+            SharedPreferences.Editor editor = preferences.edit();
+            //存入数据
+            editor.putInt("count", 1); // 存入数据
+            //提交修改
+            editor.commit();
+
+        }catch (InterruptedException e){
+
+        }
+    }
+}
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,22 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_launch_improve).setOnClickListener(this);
     }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_launch_improve) {
-            Intent intent = new Intent(this, LaunchImproveActivity.class);
-            startActivity(intent);
-        }
-    }
-
 }
